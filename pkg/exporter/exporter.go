230fca0fac01b31c41bfede61f7b830842f55779
Jeroen van Erp <jvanerp@stackstate.com>
2023-11-16 16:30:13 +0100

    Add gzip compression support
    Signed-off-by: Jeroen van Erp <jvanerp@stackstate.com>

--- pkg/exporter/exporter.go
+++ pkg/exporter/exporter.go
@@ -16,6 +16,7 @@ import (
 	"github.com/stackvista/sts-otel-bridge/pkg/collector"
 	"github.com/stackvista/sts-otel-bridge/pkg/sender/stdout"
 	"google.golang.org/grpc"
+	_ "google.golang.org/grpc/encoding/gzip"
 )
 
 type Exporter struct {
